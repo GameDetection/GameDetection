108070bd12f86420a5c0bdcbfe077cf2b0035d53
MrAylex <carmonealexandre@hotmail.fr>
2022-04-29 09:35:50 +0200

    les problèmes sont résolus, il manquait la version de regression du calcul du delta et la tanh n'était pas appliqué dans les couches cacher

--- main.cpp
+++ main.cpp
@@ -16,47 +16,27 @@ void printLayer(auto layer) {
 }
 
 int main() {
-    int tab[2] = {2,1};
-    MatrixXf input(1,2);
-    input(0,0) = 10;
-    input(0,1) = 10;
-//    input(0,2) = 10;
-//    input(0,3) = 10;
-//    input(0,4) = 10;
-//    input(0,5) = 10;
-//    input(0,6) = 10;
-//    input(0,7) = 10;
-//    input(0,8) = 10;
-//    input(0,9) = 10;
+    srand((unsigned int) time(0));
+
+    int tab[3] = {4,3,1};
+    MatrixXf input(1,4);
+    input.setConstant(10);
 
     MatrixXf Y(1,1);
-    Y(0,0) = 10;
+    Y(0,0) = 1;
 
-    Mlp_model* model = create_mlp_model(tab,2);
+    Mlp_model* model = create_mlp_model(tab,3);
 
-//    auto layerW = model->W;
-    for (int layerID = 0; layerID < model->W.size(); ++layerID) {
-        std::cout << " LayerW " << layerID << std::endl;
-        for (int neurID = 0; neurID < model->W(layerID).size(); ++neurID) {
-            std::cout << " neurW " << neurID << std::endl;
-            std::cout << model->W(layerID)(neurID) << std::endl;
-        }
-    }
-    auto layerX = model->X;
-    for (int layerID = 0; layerID < layerX.size(); ++layerID) {
-        std::cout << " LayerX " << layerID << std::endl;
-        std::cout << layerX(layerID) << std::endl;
-    }
-    VectorXf res = predict(model, input.row(0), 0);
+    VectorXf res = predict(model, input.row(0), 1);
     std::cout << "Prediction: " << res << "\n";
     auto start = std::chrono::high_resolution_clock::now();
-    train(model, input, Y, 0.0001, 100, 0);
+    train(model, input, Y, 0.0001, 40000, 1);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end-start;
     std::cout << "Training time: " << diff.count() << " s\n";
 
     std::cout << "Input: " << input.row(0) << "\n";
-    res = predict(model, input.row(0), 0);
+    res = predict(model, input.row(0), 1);
     std::cout << "Prediction: " << res << "\n";
     return 0;
 }
--- src/mlp_model.cpp
+++ src/mlp_model.cpp
@@ -2,7 +2,6 @@
 // Created by Alexandre Carmone on 02/04/2022.
 //
 #include "../inc/mlp_model.h"
-
 #include <iostream>
 
 using namespace Eigen;
@@ -13,13 +12,13 @@ using namespace Eigen;
  * @param len
  * @return
  */
-Mlp_model* create_mlp_model(int tab[], int len) {
+Mlp_model *create_mlp_model(int tab[], int len) {
     // Create a model with the given parameters
-    auto* model = new Mlp_model();
+    auto *model = new Mlp_model();
     model->inputSize = tab[0];
-    model->l.resize(len -1);
+    model->l.resize(len - 1);
     for (int i = 1; i < len; ++i) {
-        model->l(i -1 ) = tab[i];
+        model->l(i - 1) = tab[i];
     }
     model->X.resize(len);
     model->B.resize(len);
@@ -58,25 +57,28 @@ VectorXf predict(Mlp_model *model, VectorXf X, bool isClassification) {
     model->X(0) = X;
     for (int layerID = 1; layerID < model->X.size(); layerID++) {
         for (int neurID = 0; neurID < model->X(layerID).size(); neurID++) {
-            // Si nous sommes à la dernière couche et que le model est un classificateur, on applique la tangente hyperbolique
-            if (model->X.size() - 1 == layerID and isClassification) {
-                model->X(layerID)(neurID) = tanh((model->X(layerID - 1).transpose() *
-                          model->W(layerID)(neurID) +
-                          model->B(layerID)(neurID))
-                        .sum());
-            }
-            else {
-                // pour toutes les autres couches on calcule le résultat de la couche
-                model->X(layerID)(neurID) = (model->X(layerID - 1).transpose() *
-                         model->W(layerID)(neurID) +
-                         model->B(layerID)(neurID))
-                        .sum();
-            }
 
+            if (model->X.size() - 1 == layerID) {
+                if (isClassification) {
+                    model->X(layerID)(neurID) = tanh((model->X(layerID - 1).transpose() *
+                                                      model->W(layerID)(neurID) +
+                                                      model->B(layerID)(neurID))
+                                                             .sum());
+                } else {
+                    model->X(layerID)(neurID) = ((model->X(layerID - 1).transpose() *
+                                                  model->W(layerID)(neurID) +
+                                                  model->B(layerID)(neurID))
+                            .sum());
+                }
+            } else {
+                model->X(layerID)(neurID) = tanh((model->X(layerID - 1).transpose() *
+                                                  model->W(layerID)(neurID) +
+                                                  model->B(layerID)(neurID))
+                                                         .sum());
+            }
         }
     }
     VectorXf a = model->X(model->X.size() - 1);
-//    std::cout << " res ="<< a << std::endl;
 
     return a;
 }
@@ -90,61 +92,49 @@ VectorXf predict(Mlp_model *model, VectorXf X, bool isClassification) {
  * @param epochs
  * @param isClassification
  */
-void train(Mlp_model* model, MatrixXf inputs, MatrixXf Y, float learningRate, int epochs, bool isClassification) {
+void train(Mlp_model *model, MatrixXf inputs, MatrixXf Y, float learningRate, int epochs, bool isClassification) {
     int k;
     VectorXf Xk;
     VectorXf Yk;
     for (int i = 0; i < epochs; ++i) {
+//        system("cls");
+//        std::cout << "Epoch: "<< i << std::endl;
         k = rand() % inputs.rows();
         Xk = inputs.row(k);
         Yk = Y.row(k);
 
+
         predict(model, Xk, isClassification);
         //On parcours les couche du model
-        for (int layerID = model->W.size() - 1; layerID >= 0 ; --layerID) {
+        for (int layerID = model->W.size() - 1; layerID >= 0; --layerID) {
+
             VectorXf One(model->X(layerID).size());
             One.setOnes();
-            // si on est dans la derniere couche
-             // Ici on calcule de le Delta de la dernière couche
-             //TODO: en fait c'est sans doute ici que ça plante
-
+            // Ici on calcule de le Delta de la dernière couche
             if (layerID == model->W.size() - 1) {
-//                auto m = One - model->X(layerID).cwiseProduct(model->X(layerID));
-//                std::cout << "m = " << m << std::endl;
-//                auto n = model->X(layerID) - Yk;
-//                std::cout << "test X^2 = " << model->X(layerID).cwiseProduct(model->X(layerID)) << std::endl;
-//                auto test = One - (model->X(layerID).cwiseProduct(model->X(layerID)));//
-//                auto test2 = model->X(layerID) - Yk;
-//                std::cout << "test 1 - x^2 = " << test << std::endl;
-//                std::cout << "test x - y = " << test2 << std::endl;
-                model->Delta(layerID) = (One -
-                        model->X(layerID).cwiseProduct(model->X(layerID))).cwiseProduct(model->X(layerID) - Yk);
-//                std::cout << "Delta test = " << (One -
-//                                                 model->X(layerID).cwiseProduct(model->X(layerID))).cwiseProduct(model->X(layerID) - Yk) << std::endl;
-                std::cout << "Delta L = " << model->Delta(layerID) << std::endl;
+                if (isClassification) {
+                    //on fait le calcul de la dernière couche en fonction de la classification
+                    model->Delta(layerID) = (One - model->X(layerID).cwiseProduct(model->X(layerID))).cwiseProduct(
+                            model->X(layerID) - Yk);
+                } else {
+                    model->Delta(layerID) = model->X(layerID) - Yk;
+                }
             }
-            // pour les autres couches
+                // pour les autres couches
             else {
                 VectorXf res(model->W(layerID).size());
                 res.setZero();
                 // pour chaque neuronne dans la couche
-                // cette partie fonctionne très mal
-
-                 // ensuite on calcule le delta pour toutes les autres couches
-                 // TODO: regarder cette partie pour que le delta soit correct
                 for (int neurID = 0; neurID < model->Delta(layerID + 1).size(); ++neurID) {
                     res += model->W(layerID + 1)(neurID) * model->Delta(layerID + 1)(neurID);
                 }
+                //on calcule le delta de la couche
                 model->Delta(layerID) = (One - model->X(layerID).cwiseProduct(model->X(layerID))).cwiseProduct(res);
-//                std::cout << "Delta= " << model->Delta(layerID) << std::endl;
-
             }
             if (layerID > 0) {
                 //On fait la mise à jour des poids
                 for (int neurID = 0; neurID < model->W(layerID).size(); ++neurID) {
-                    std::cout << "W avant = " << model->W(layerID)(neurID) << std::endl;
                     model->W(layerID)(neurID) -= learningRate * model->X(layerID - 1) * model->Delta(layerID)(neurID);
-                    std::cout << "W apres = " << model->W(layerID)(neurID) << std::endl;
                 }
             }
         }
